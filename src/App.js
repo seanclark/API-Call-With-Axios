5939d855d1f77ade36ce183448dc4243b461876a
Sean Clark <sclark9999@verizon.net>
2021-08-12 19:57:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Container from '@material-ui/core/Container';
 function App() {
 return (
   <React.Fragment>
-      <CssBaseline />
+        <CssBaseline />
       {
         <Container maxWidth="sm">
          <h1>POST SEARCH</h1>
