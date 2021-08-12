6de2fdbe82e915c14fe8937e3b53de6eae01292e
Sean Clark <sclark9999@verizon.net>
2021-08-12 19:57:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import './App.css';
 import FetchData from './FetchData';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
@@ -18,34 +17,4 @@ return (
    )
   } 
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /*axios.defaults.baseURL = "https://reqres.in/api";
-  const getData = ()=> {
-  axios.get("/users")
-  .then(res => console.log(res.data.data));
-  }
-  const postData = () => {
-  axios.post("/users")
-  .then((res) => console.log(res))
-  .catch((res) => console.log(res));
-  } 
-
-  return (
-    <div className="App">
-      <h1>User Directory</h1>
-      <button onClick = {getData}>Get User Records</button>
-      <button onClick = {postData}>Post New Record</button>
-       </div>
-  )
-  }*/
-export default App;
+ export default App;
