d18473b36277055e5dc680121b801a2e2cdd193a
Sean Clark <seanclark@Seans-Mac-Pro.fios-router.home>
2021-08-20 12:05:40 -0400

    Deleted older code in App.js

--- src/App.js
+++ src/App.js
@@ -14,33 +14,4 @@ return (
   } 
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /*axios.defaults.baseURL = "https://reqres.in/api";
-  const getData = ()=> {
-  axios.get("/users")
-  .then(res => console.log(res.data.data));
-  }
-  const postData = () => {
-  axios.post("/users")
-  .then((res) => console.log(res))
-  .catch((res) => console.log(res));
-  } 
-
-  return (
-    <div className="App">
-      <h1>User Directory</h1>
-      <button onClick = {getData}>Get User Records</button>
-      <button onClick = {postData}>Post New Record</button>
-       </div>
-  )
-  }*/
 export default App;
